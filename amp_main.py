d6238bd54065b4b1aad5da73148ce1e7d1ecfb3d
aaron545 <aaronmcyu@gmail.com>
2024-11-21 14:28:56 +0800

    first upload

---
+++ amp_main.py
@@ -0,0 +1,298 @@
+from DrissionPage import ChromiumPage, ChromiumOptions
+from DrissionPage.common import By
+from enum import Enum, IntEnum, auto
+from datetime import datetime
+
+import pytz
+import os
+import atexit
+import time
+import argparse
+import json
+
+def msgLogger(msg):
+    # 取得當前時間並設置時區為台北
+    date = datetime.now(pytz.timezone('Asia/Taipei'))
+    
+    print('--------------------------')
+    print(date.strftime('%Y/%m/%d %I:%M:%S %p'))
+    print(msg)
+    print('--------------------------')
+
+def close_driver(page):
+	page.quit()
+	print("close!")
+
+def set_SA_value(features, li_elements):
+	for key, value in features.items():
+		try:	
+			# for textbox
+			element = li_elements[Li_SA[key]].ele('tag:input@@type:text', timeout = 0.1)
+			element.clear(by_js = True)
+			element.input(value)
+		except:
+			# for selectmenu
+			try:
+				li_elements[Li_SA[key]].ele('tag:button', timeout = 0.1).run_js('this.click()', timeout = 0.1)
+				time.sleep(0.2)
+				page.ele('@class:dropdown-menu show').ele(f'tag:a@@text():{value}').click()
+				
+			except:
+				pass
+		finally:
+			time.sleep(0.1)
+	if li_elements[Li_SA.ADMIN_STATE].ele("@@class:text-truncate@@class:badge-pill").text != "Product":
+		li_elements[Li_SA.ADMIN_STATE].ele(".ng-star-inserted").click()
+
+
+def get_SA_value():
+	if len(li_elements) == len(Li_SA):
+		for feature in Li_SA:
+			try:	
+				# for textbox
+				element = li_elements[feature.value].ele('tag:input@@type:text', timeout = 0.1)
+				value = element.run_js('return this.value')
+				print(feature.name, ":", value)
+			except:
+				# for selectmenu
+				try:
+					element = li_elements[feature.value].ele('tag:button', timeout = 0.1)
+					print(feature.name, ":", element.text)
+				except:
+					pass
+			finally:
+				pass
+	else:
+		print('Length is inconsistent, please fix the code.')
+
+def set_DEPLOY_value(features, li_elements):
+	is3ms = False
+	for key, value in features.items():
+		li_elements[Li_DEPLOY[key]].ele('tag:button', timeout = 0.1).run_js('this.click()', timeout = 0.1)
+		time.sleep(0.2)
+		page.ele('@class:dropdown-menu show').ele(f'tag:a@@text():{value}').click()
+		if key == "TIMESLOT" and value == "DDDDDDDSUU":
+			is3ms = True
+
+	msgLogger("check 3ms timing offset...")
+	time.sleep(0.5)
+	offset_element = page.ele('tag:ul@class:list-group').eles('tag:li')[Li_DEPLOY.OFFSET]
+	isOffset = True if offset_element.ele("@@class:text-truncate@@class:badge-pill").text == "Product" else False
+	if not is3ms and isOffset:
+		offset_element.ele(".ng-star-inserted").click()
+
+class MENU(IntEnum):
+	LIVE_UPDATE = 0
+	PROVISIONING = auto()
+	REBOOT = auto()
+	UPGRADE_FIRMWARE = auto()
+
+class TAB(IntEnum):
+	SA = 2
+	NEIGHBOR = auto()
+	LTE_NEIGHBOR = auto()
+	CONFIG_NEIGHBOR = auto()
+	HO_EVENT = auto()
+	DEPLOYMENT = auto()
+	PM = auto()
+
+class Li_SA(IntEnum):
+	GNB_ID = 0
+	GNB_ID_LENGTH = auto()
+	CELL_ID = auto()
+	TAC = auto()
+	MCC = auto()
+	MNC = auto()
+	SST = auto()
+	SD = auto()
+	NSSAI = auto()
+	AMF_IP = auto()
+	PCI = auto()
+	EPSFB = auto()
+	MODULATION = auto()
+	LAYER = auto()
+	DRMS = auto()
+	UE_INACTIVITY = auto()
+	POWER = auto()
+	ADMIN_STATE = auto()
+
+class Li_DEPLOY(IntEnum):
+	OFFSET = 0
+	DEPLOYMENT_MODE = auto()
+	NR_BAND = auto()
+	BANDWIDTH = auto()
+	NRARFCN = auto()
+	TIMESLOT = auto()
+
+class Config:
+	def __init__(self,**kwargs):
+		# 動態設置屬性
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+default_provision = ["2120_BASIC.json", "2120_QAM256_2L.json"]
+default_radio = "radio.json"
+
+parser = argparse.ArgumentParser(description="Load configuration files")
+parser.add_argument("-p", "--provisions", nargs="*",default=default_provision, help="List of provision files to load")
+parser.add_argument("-r", "--radio", nargs="?", default=default_radio, help="radio file to load")
+args = parser.parse_args()
+
+provision = {}
+for file in args.provisions:
+	with open(file, 'r') as f:
+		provision.update(Config(**json.load(f)).__dict__)
+with open(args.radio, 'r') as f:
+	radio = Config(**json.load(f)).__dict__
+
+
+with open("config.json", 'r') as f:
+	data = json.load(f)
+config = Config(**data)
+
+download_directory = os.path.dirname(__file__)
+co = ChromiumOptions()
+co.set_pref('download.default_directory', download_directory)
+co.set_argument('--window-size', '1920,1080')
+co.set_browser_path('../chrome-win/chrome.exe')
+# co.set_browser_path("C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
+co.ignore_certificate_errors()
+co.incognito()
+# co.headless()
+
+page = ChromiumPage(co)
+# atexit.register(close_driver, page)
+
+page.get(config.login_url, timeout = 10)
+msgLogger("waiting for get amp page...")
+
+# -----------------------login-----------------------
+
+# input user
+page.ele('tag:input@formcontrolname=email').input(config.user)
+
+# input password
+page.ele('tag:input@formcontrolname=password').input(config.amp_password)
+
+# sign in
+page.ele('tag:button@@text():Sign in').click()
+msgLogger("waiting for login...")
+
+
+# -----------------------searchSN-----------------------
+time.sleep(1)
+try:
+	button_device = page.ele('@class:avatar bg-light-primary p-50 m-0')
+except:
+	button_device = page.ele('tag:a@href=#/devices', timeout = 2)
+
+button_device.click()
+msgLogger("switch to devices page...")
+
+
+serial_number = config.serial_number
+try:
+	tb_serial_number = page.ele('tag:datatable-header-cell@style:211.962px').ele('tag:input@type=text')
+except:
+	tb_serial_number = page.ele('tag:datatable-header-cell@style:204.038px').ele('tag:input@type=text')
+
+msgLogger("searching SN...")
+tb_serial_number.input(serial_number)
+
+time.sleep(2)
+# loc = (By.XPATH, f'//div[@class="text-truncate ng-star-inserted" and contains(text(), "{serial_number}")]')
+# link_small_cell = page.ele(loc)
+link_small_cell = page.ele(f'@@class:text-truncate ng-star-inserted@@text():{serial_number}')
+link_small_cell.click()
+
+button_menu = page.eles('@@class:btn@@class:btn-primary@@class:ng-star-inserted')
+button_menu[MENU.PROVISIONING].click()
+msgLogger("opening the provisoning page...")
+time.sleep(2)
+
+
+# tab = page.eles('@class:nav-item ng-star-inserted')
+
+# print("before:")
+# get_SA_value()
+# ----------------------set feature value----------------------
+# for textbox
+
+# element = li_elements[0].ele('tag:input@@type:text', timeout = 0.1)
+# element.clear(by_js = True)
+# value = element.run_js('return this.value')
+# # print(Li_SA(0).name, ":", value)
+# time.sleep(0.2)
+# element.input('7128')
+# value = element.run_js('return this.value')
+# # print(Li_SA(0).name, ":", value)
+
+# # for selectmenu
+# value = 'QAM256'
+# li_elements[12].ele('tag:button', timeout = 0.1).run_js('this.click()', timeout = 0.1)
+# page.ele(f'xpath://a[contains(text(), "{value}")]')
+# element = page.ele(f'tag:a@@text():{value}')
+# element.click()
+
+# if li_elements[Li_SA.ADMIN_STATE].ele("@@class:text-truncate@@class:badge-pill").text != "Product":
+# 	li_elements[Li_SA.ADMIN_STATE].ele(".ng-star-inserted").click()
+
+
+msgLogger("setting the SA configs...")
+tab = page.eles('@class:nav-item ng-star-inserted')
+tab[TAB.SA].click()
+time.sleep(0.5)
+li_elements = page.ele('tag:ul@class:list-group').eles('tag:li')
+set_SA_value(provision, li_elements)
+
+msgLogger("setting the radio config...")
+tab = page.eles('@class:nav-item ng-star-inserted')
+tab[TAB.DEPLOYMENT].click()
+time.sleep(0.5)
+li_elements = page.ele('tag:ul@class:list-group').eles('tag:li')
+set_DEPLOY_value(radio, li_elements)
+
+# print("\nafter:")
+# get_SA_value()
+
+# ----------------------print feature value----------------------
+
+# if len(li_elements) == len(Li_SA):
+# 	for feature in Li_SA:
+# 		try:	
+# 			# for textbox
+# 			element = li_elements[feature.value].ele('tag:input@@type:text', timeout = 0.1)
+# 			value = element.run_js('return this.value')
+# 			print(feature.name, ":", value)
+# 		except:
+# 			# for selectmenu
+# 			try:
+# 				element = li_elements[feature.value].ele('tag:button', timeout = 0.1)
+# 				print(feature.name, ":", element.text)
+# 			except:
+# 				pass
+# 		finally:
+# 			pass
+# else:
+# 	print('Length is inconsistent, please fix the code.')
+
+
+# download csv
+# page.ele('tag:label@for=download-default').click()
+# time.sleep(2)
+
+# save provision
+msgLogger("saving the provision page...")
+page.ele('tag:button@@text():Apply').click()
+
+# close provision
+# msgLogger("closing the provision page...")
+# button_close = page.eles('.close')
+# button_close[1].click()
+
+time.sleep(1)
+# button_menu = page.eles('@@class:btn@@class:btn-primary@@class:ng-star-inserted')
+# button_menu[menu.REBOOT].click()
+# page.ele('tag:button@@text():OK').click()
+
+# time.sleep(200)
---
+++ web_main.py
@@ -0,0 +1,265 @@
+from DrissionPage import ChromiumPage, ChromiumOptions
+from enum import Enum, IntEnum, auto
+from datetime import datetime
+
+import pytz
+import os
+import atexit
+import time
+import argparse
+import json
+import re
+
+def msgLogger(msg):
+    # 取得當前時間並設置時區為台北
+    date = datetime.now(pytz.timezone('Asia/Taipei'))
+    
+    print('--------------------------')
+    print(date.strftime('%Y/%m/%d %I:%M:%S %p'))
+    print(msg)
+    print('--------------------------')
+
+
+class Config:
+	def __init__(self,**kwargs):
+		# 動態設置屬性
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+class ASK_nonIPSEC_SD_select(IntEnum):
+	IPSecTunnel = 0
+	SD = auto()
+
+class ASK_IPSEC_SD_select(IntEnum):
+	IPSecTunnel = 0
+	AuthenticationType = auto()
+	SD = auto()
+
+default_provision = ["2120_BASIC.json", "2120_QAM256_2L.json", "radio.json"]
+
+parser = argparse.ArgumentParser(description="Load configuration files")
+parser.add_argument("files", nargs="*",default=default_provision, help="List of configuration files to load")
+args = parser.parse_args()
+
+qam_files = [file for file in args.files if "QAM" in file]
+other_files = [file for file in args.files if "QAM" not in file]
+
+provision = {}
+for file in other_files + qam_files:
+    with open(file, 'r') as f:
+        provision.update(Config(**json.load(f)).__dict__)
+
+with open("config.json", 'r') as f:
+	config = Config(**json.load(f))
+
+CU_list = ["gnb_n2_ip", "gnb_n3_ip", "gnb_id", "gnb_id_length", "cell_id", "tac", "mcc", "mnc", "amf_ip"]
+DU_list = ["sst", "sd", "nr_band", "physical_cell_id", "modulation", "layer", "drms","bandwidth", "nrarfcn", "timeslot"]
+
+textbox_list = ["gnb_n2_ip", "gnb_n3_ip", "gnb_id", "gnb_id_length", "cell_id", "tac", "mcc", "mnc", "amf_ip", "sst", "physical_cell_id"]
+checkbox_list = ["modulation", "layer", "drms"]
+selectmenu_list = ["nr_band"]
+selectmenu_profile_list = ["bandwidth", "nrarfcn", "timeslot"]
+
+co = ChromiumOptions()
+co.set_argument('--window-size', '1920,1080')
+co.set_browser_path('../chrome-win/chrome.exe')
+# co.set_browser_path("C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
+co.set_timeouts(base=10)
+co.ignore_certificate_errors()
+co.incognito()
+# co.headless()
+
+page = ChromiumPage(co)
+root = config.web_root
+# root = "https://10.1.108.129"
+
+if page.url != root:
+	page.get(root)
+	time.sleep(1)
+
+# ----------------------Login----------------------
+msgLogger("waiting for login...")
+try:
+	page.ele('#password').input(config.web_password)
+	page.ele("#btn_login").click()
+except:
+	msgLogger("already logined")
+
+# ----------------------ReminderBlock----------------------
+msgLogger(f"waiting for get {root}/home...")
+while page.url != root+"/home":
+	time.sleep(1)
+time.sleep(4)
+
+msgLogger("try to close reminderBlock...")
+
+if len(page.ele("#popup_clone").eles("tag:div",timeout=1)) == 0:
+	msgLogger("not need to close")
+else:
+	reminderBlock = page.ele("#popup_clone").ele(".wrap_pop1 modal-content", timeout=5)
+	reminderBlock.ele("tag:a@@text():OK").click()
+	msgLogger("OK")
+
+# ----------------------Switch to configuration----------------------
+msgLogger("switch to gNB configuration...")
+while page.url != root+"/configuration/gNB":
+	page.ele("tag:a@@href=/configuration/gNB").click()
+	time.sleep(2)
+
+# ----------------------Configuration----------------------
+# switch gNB CU and DU
+# page.ele("tag:div@@class:switchBtn").click()
+
+
+# selectmenu 
+# SD = "66051"
+# SD = "Disabled"
+
+# page.eles(".SD_select css-b62m3t-container")[SD_select.SD].click()
+# if SD != "Disabled":
+# 	page.ele(". css-4o2p2z-menu").ele("text:Enabled").click()
+# 	page.ele("#sd").clear()
+# 	page.ele("#sd").input(SD)
+# else:
+# 	page.ele(". css-4o2p2z-menu").ele("text:Disabled").click()
+
+# textbox
+# gnb_id = page.ele("#gnb_id")
+# gnb_id.clear()
+# gnb_id.input("7128")
+
+# checkbox
+# modulation = "QAM256"
+# layer = "2"
+# drms = "pos2"
+# page.ele(f"tag:label@@for:{modulation}").click()
+
+# if layer == "1":
+# 	page.ele(f"tag:label@@for:One layer").click()
+# else:
+# 	page.ele(f"tag:label@@for:Two layer").click()
+
+# if drms == "pos1":
+# 	page.ele(f"tag:label@@for:Pos1").click()
+# else:
+# 	page.ele(f"tag:label@@for:Pos2").click()
+
+
+# ----------------------Modify parameters----------------------
+
+msgLogger("ready to start modifying parameters")
+time.sleep(10)
+
+# current is CU or DU page
+styleAttr = page.ele("tag:div@@class:switchBtn").attr("style")
+styleValue = re.search(r"left:\s*(\d+)px", styleAttr).group(1)
+isCU = True if int(styleValue) > 50 else False
+
+# switch between remote and local
+Method = "local"
+page.ele(f"tag:label@@for:{Method}").click()
+
+isIPsec = ''
+isASK = False if len(page.eles(".SD_select css-b62m3t-container")) == 1 else True
+print("isASK =", isASK)
+if isASK:
+	# isIPsec = True if page.eles(".SD_select css-b62m3t-container")[0].text == "Enabled" else False
+	# print("isIPsec =", isIPsec)
+	if page.eles(".SD_select css-b62m3t-container")[0].text != "Enabled":
+		page.eles(".SD_select css-b62m3t-container")[0].click()
+		page.ele(". css-4o2p2z-menu").ele("text:Enabled").click()
+		time.sleep(0.5)
+
+	page.ele("#security_gateway").clear()
+	page.ele("#security_gateway").input("211.75.141.110")
+	page.ele("#ipsec_right_subnet").clear()
+	page.ele("#ipsec_right_subnet").input("172.29.0.0/22")
+
+
+for key, value in provision.items():
+	if key != "EPSFB" and key != "POWER":
+		if (key.lower() in CU_list) != isCU:
+			page.ele("tag:div@@class:switchBtn").click()
+			isCU = not isCU
+
+		if key.lower() in textbox_list:
+			print(key.lower(), "=", value)
+			gnb_id = page.ele(f"#{key.lower()}")
+			gnb_id.clear()
+			gnb_id.input(value)
+
+		elif key == "SD":
+			if not isASK:
+				page.ele("@@class:SD_select").click()
+			# elif isIPsec:
+			# 	page.eles("@@class:SD_select")[ASK_IPSEC_SD_select.SD].click()
+			# else:
+			# 	page.eles("@@class:SD_select")[ASK_nonIPSEC_SD_select.SD].click()
+			else :
+				page.eles("@@class:SD_select")[ASK_IPSEC_SD_select.SD].click()
+
+			if value != "Disabled":
+				print(key.lower(), "= Enabled,", value)
+				page.ele(". css-4o2p2z-menu").ele("text:Enabled").click()
+				page.ele("#sd").clear()
+				page.ele("#sd").input(value)
+			else:
+				print(key.lower(), "=", value)
+				page.ele(". css-4o2p2z-menu").ele("text:Disabled").click()
+
+		elif key == "PCI":
+			print("physical_cell_id =", value)
+			gnb_id = page.ele(f"#physical_cell_id")
+			gnb_id.clear()
+			gnb_id.input(value)
+
+		elif key.lower() in checkbox_list:
+			if key.lower() == "modulation":
+				print("modulation =", value)
+				page.ele(f"tag:label@@for:{value}").click()
+			elif key.lower() == "layer":
+				print("layer =", value)
+				if value == "1":
+					page.ele(f"tag:label@@for:One layer").click()
+				else:
+					page.ele(f"tag:label@@for:Two layer").click()
+			elif key.lower() == "drms":
+				print("drms =", value)
+				if value == "pos1":
+					page.ele(f"tag:label@@for:Pos1").click()
+				else:
+					page.ele(f"tag:label@@for:Pos2").click()
+
+		elif key.lower() in selectmenu_list:
+			print(key.lower(), "=", value)
+			page.ele(f"@@class:{key.lower()}").click()
+			page.ele(". css-4o2p2z-menu").ele(f"text:{value}").click()
+
+		elif key.lower() in selectmenu_profile_list:
+			print(key.lower(), "=", value)
+			if key.lower() == "nrarfcn":
+				page.ele(f"@@class:nrArfcn_profile").click()
+			elif key.lower() == "timeslot":
+				page.ele(f"@@class:timeSlot_profile").click()
+			else:
+				page.ele(f"@@class:{key.lower()}_profile").click()
+			page.ele(". css-4o2p2z-menu").ele(f"text:{value}").click()	
+msgLogger("finish!!!")
+
+# save config
+msgLogger("save config...")
+page.ele("#id_Save").click()
+time.sleep(1)
+
+cfm_box = page.ele("#cfm_box")
+if cfm_box.attr("aria-hidden") != "true":
+	cfm_box.ele("tag:a@@name:box_ok").click()
+	time.sleep(1)
+msgLogger("waiting for cfm_box visible...")
+cfm_box = page.ele("#cfm_box")
+while cfm_box.attr("aria-hidden") == "true":
+	cfm_box = page.ele("#cfm_box")
+	time.sleep(1)
+msgLogger("cfm_box is visible...")
+cfm_box.ele("tag:a@@name:box_x").click()
+msgLogger("finish! reboot now...")
